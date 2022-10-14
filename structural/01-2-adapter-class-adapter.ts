6228e60bc4f738c67581f20976fcfdb71e2a44ef
Nitirat Saengchaiarun <nitirat@praneat.com>
2022-10-14 16:43:33 +0700

    feat: 🎸 Class adapter with mixin

---
+++ structural/01-2-adapter-class-adapter.ts
@@ -0,0 +1,55 @@
+/**
+ * 02- Squre Pegs and Round Holes with Mixin
+ */
+export namespace Adapter2 {
+    class RoundHole {
+        constructor(private _radius: number) {}
+
+        public fit(peg: RoundPeg): boolean {
+            return this._radius > peg.radius
+        }
+    }
+
+    class RoundPeg {
+        constructor(protected _radius: number) {}
+
+        public get radius() {
+            return this._radius
+        }
+    }
+
+    type Constructor = new (...args: any[]) => {}
+
+    function SquarePegMixin<TBase extends Constructor>(Base: TBase) {
+        return class SquarePeg extends Base {
+            private _width: number = 0
+
+            public setWidth(width: number) {
+                this._width = width
+            }
+
+            public get width() {
+                return this._width
+            }
+
+            public get radius() {
+                return (this.width * Math.sqrt(2)) / 2
+            }
+        }
+    }
+
+    //IIFE (Immediately-Invoked Function Expression)
+    ;(() => {
+        const roundHole = new RoundHole(5)
+
+        const roundPeg = new RoundPeg(6)
+        console.log(roundHole.fit(roundPeg))
+
+        const SquarePegAdapter = SquarePegMixin(RoundPeg)
+        const squarePegAdapter = new SquarePegAdapter(0)
+        squarePegAdapter.setWidth(8)
+
+        console.log(squarePegAdapter.radius)
+        console.log(roundHole.fit(squarePegAdapter))
+    })()
+}
